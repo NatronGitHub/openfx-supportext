bb2099713eea201a2bf6713859602aa4f921c937
Frédéric Devernay <frederic.devernay@m4x.org>
2014-08-24 11:04:17 +0200

    PixelCopier: implement variants

--- ofxsCopier.h
+++ ofxsCopier.h
@@ -18,18 +18,46 @@ namespace OFX {
 // Base class for the RGBA and the Alpha processor
 
 // template to do the RGBA processing
-template <class PIX, int nComponents, int maxValue, bool masked>
+template <class PIX, int nComponents, int maxValue>
 class PixelCopier : public OFX::PixelProcessorFilterBase {
 public:
     // ctor
     PixelCopier(OFX::ImageEffect &instance)
     : OFX::PixelProcessorFilterBase(instance)
-    {}
+    {
+    }
 
     // and do some processing
     void multiThreadProcessImages(OfxRectI procWindow)
     {
         int rowBytes = sizeof(PIX) * nComponents * (procWindow.x2 - procWindow.x1);
+        for(int y = procWindow.y1; y < procWindow.y2; ++y) {
+            if(_effect.abort()) {
+                break;
+            }
+
+            PIX *dstPix = (PIX *) getDstPixelAddress(procWindow.x1, y);
+            assert(dstPix);
+
+            const PIX *srcPix = (const PIX *) getSrcPixelAddress(procWindow.x1, y);
+            assert(srcPix);
+            std::memcpy(dstPix, srcPix, rowBytes);
+        }
+    }
+};
+
+template <class PIX, int nComponents, int maxValue, bool masked>
+class PixelCopierMaskMix : public OFX::PixelProcessorFilterBase {
+public:
+    // ctor
+    PixelCopierMaskMix(OFX::ImageEffect &instance)
+    : OFX::PixelProcessorFilterBase(instance)
+    {
+    }
+
+    // and do some processing
+    void multiThreadProcessImages(OfxRectI procWindow)
+    {
         float tmpPix[nComponents];
         for(int y = procWindow.y1; y < procWindow.y2; ++y) {
             if(_effect.abort()) {
@@ -39,38 +67,92 @@ public:
             PIX *dstPix = (PIX *) getDstPixelAddress(procWindow.x1, y);
             assert(dstPix);
 
-            if (!masked) {
-                const PIX *srcPix = (const PIX *) getSrcPixelAddress(procWindow.x1, y);
-                assert(srcPix);
-                if (_mix == 1) {
-                    std::memcpy(dstPix, srcPix, rowBytes);
+            for (int x = procWindow.x1; x < procWindow.x2; x++) {
+                const PIX *origPix = (const PIX *)  (_origImg ? _origImg->getPixelAddress(x, y) : 0);
+                const PIX *srcPix = (const PIX *) getSrcPixelAddress(x, y);
+                if (srcPix) {
+                    std::copy(srcPix, srcPix + nComponents, tmpPix);
                 } else {
-                    for (int x = procWindow.x1; x < procWindow.x2; x++) {
-                        const PIX *origPix = (const PIX *)  (_origImg ? _origImg->getPixelAddress(x, y) : 0);
-                        const PIX *srcPix = (const PIX *) getSrcPixelAddress(x, y);
-                        if (srcPix) {
-                            std::copy(srcPix, srcPix + nComponents, tmpPix);
-                        } else {
-                            std::fill(tmpPix, tmpPix + nComponents, 0.); // no src pixel here, be black and transparent
-                        }
-                        ofxsMaskMixPix<PIX, nComponents, maxValue, masked>(tmpPix, x, y, origPix, _doMasking, _maskImg, _mix, _maskInvert, dstPix);
-                        // increment the dst pixel
-                        dstPix += nComponents;
-                    }
+                    std::fill(tmpPix, tmpPix + nComponents, 0.); // no src pixel here, be black and transparent
                 }
-            } else {
-                for (int x = procWindow.x1; x < procWindow.x2; x++) {
-                    const PIX *origPix = (const PIX *)  (_origImg ? _origImg->getPixelAddress(x, y) : 0);
-                    const PIX *srcPix = (const PIX *) getSrcPixelAddress(x, y);
-                    if (srcPix) {
-                        std::copy(srcPix, srcPix + nComponents, tmpPix);
-                    } else {
-                        std::fill(tmpPix, tmpPix + nComponents, 0.); // no src pixel here, be black and transparent
-                    }
-                    ofxsMaskMixPix<PIX, nComponents, maxValue, masked>(tmpPix, x, y, origPix, _doMasking, _maskImg, _mix, _maskInvert, dstPix);
-                    // increment the dst pixel
-                    dstPix += nComponents;
+                ofxsMaskMixPix<PIX, nComponents, maxValue, masked>(tmpPix, x, y, origPix, _doMasking, _maskImg, _mix, _maskInvert, dstPix);
+                // increment the dst pixel
+                dstPix += nComponents;
+            }
+        }
+    }
+};
+
+template <class SRCPIX, int nComponents, int srcMaxValue, class DSTPIX, int dstMaxValue>
+class PixelCopierUnPremult : public OFX::PixelProcessorFilterBase {
+public:
+    // ctor
+    PixelCopierUnPremult(OFX::ImageEffect &instance)
+    : OFX::PixelProcessorFilterBase(instance)
+    {
+        assert(nComponents == 3 || nComponents == 4);
+    }
+
+    // and do some processing
+    void multiThreadProcessImages(OfxRectI procWindow)
+    {
+        float unpPix[4];
+        for(int y = procWindow.y1; y < procWindow.y2; ++y) {
+            if(_effect.abort()) {
+                break;
+            }
+
+            DSTPIX *dstPix = (DSTPIX *) getDstPixelAddress(procWindow.x1, y);
+            assert(dstPix);
+
+            for (int x = procWindow.x1; x < procWindow.x2; x++) {
+                const SRCPIX *srcPix = (const SRCPIX *) getSrcPixelAddress(x, y);
+                ofxsUnPremult<SRCPIX, nComponents, srcMaxValue>(srcPix, unpPix, _premult, _premultChannel);
+                for (int c = 0; c < nComponents; ++c) {
+                    float v = unpPix[c] * dstMaxValue;
+                    dstPix[c] = DSTPIX(ofxsClampIfInt<dstMaxValue>(v, 0, dstMaxValue));
                 }
+                // increment the dst pixel
+                dstPix += nComponents;
+            }
+        }
+    }
+};
+
+// template to do the RGBA processing
+template <class SRCPIX, int nComponents, int srcMaxValue, class DSTPIX, int dstMaxValue>
+class PixelCopierPremultMaskMix : public OFX::PixelProcessorFilterBase {
+public:
+    // ctor
+    PixelCopierPremultMaskMix(OFX::ImageEffect &instance)
+    : OFX::PixelProcessorFilterBase(instance)
+    {
+        assert(nComponents == 3 || nComponents == 4);
+    }
+
+    // and do some processing
+    void multiThreadProcessImages(OfxRectI procWindow)
+    {
+        float unpPix[4];
+        if (nComponents == 3) {
+            unpPix[3] = 0.;
+        }
+        for(int y = procWindow.y1; y < procWindow.y2; ++y) {
+            if(_effect.abort()) {
+                break;
+            }
+
+            DSTPIX *dstPix = (DSTPIX *) getDstPixelAddress(procWindow.x1, y);
+            assert(dstPix);
+
+            for (int x = procWindow.x1; x < procWindow.x2; x++) {
+                const SRCPIX *srcPix = (const SRCPIX *) getSrcPixelAddress(x, y);
+                for (int c = 0; c < nComponents; ++c) {
+                    unpPix[c] = srcPix[c] / (double)srcMaxValue;
+                }
+                ofxsPremultMaskMixPix<DSTPIX, nComponents, dstMaxValue, true>(unpPix, _premult, _premultChannel, x, y, srcPix, _doMasking, _maskImg, _mix, _maskInvert, dstPix);
+                // increment the dst pixel
+                dstPix += nComponents;
             }
         }
     }
@@ -82,7 +164,8 @@ public:
     // ctor
     BlackFiller(OFX::ImageEffect &instance)
     : OFX::PixelProcessorFilterBase(instance)
-    {}
+    {
+    }
     
     // and do some processing
     void multiThreadProcessImages(OfxRectI procWindow)
