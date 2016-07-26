52f74e9577ce51a235c9219fc8bf8344808775a8
Alexandre Gauthier <alexandre230@msn.com>
2016-07-26 11:56:34 +0200

    SupportEXT: add glad, ofxsOGLFunctions_gl and ofxsOGLFunctions_mesa (copied from Natron) and ofxsOGLUtilities as well as a opengl-readme. This is to help doing OpenGL rendering with plug-ins.

---
+++ ofxsOGLFunctions_gl.cpp
@@ -0,0 +1,13 @@
+/*THIS FILE WAS GENERATED AUTOMATICALLY FROM glad.h, DO NOT EDIT*/
+
+
+
+#include "ofxsOGLFunctions_gl.h"
+
+
+namespace OFX {
+
+template class OSGLFunctions<true>;
+
+} // namespace OFX 
+
---
+++ ofxsOGLFunctions_mesa.cpp
@@ -0,0 +1,13 @@
+/*THIS FILE WAS GENERATED AUTOMATICALLY FROM glad.h, DO NOT EDIT*/
+
+
+
+#include "ofxsOGLFunctions_mesa.h"
+
+
+namespace OFX {
+
+template class OSGLFunctions<false>;
+
+} // namespace OFX
+
